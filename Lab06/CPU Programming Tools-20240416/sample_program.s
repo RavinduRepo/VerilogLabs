e57f38968cf3de53dc47571ef430ac6a09891752
RaviAlpha <ravindupathirage12345@gmail.com>
2024-05-25 18:07:49 +0530

    Task05 completed and adder and shifters are done(assembler is only convert to 2s complemet if needed)

--- Lab06/CPU Programming Tools-20240416/sample_program.s
+++ Lab06/CPU Programming Tools-20240416/sample_program.s
@@ -1,10 +1,10 @@
-loadi 1 0xFD // r1 = 3
-loadi 2 0xFC // r2 = 4
+loadi 1 0x03 // r1 = 3
+loadi 2 0x04 // r2 = 4
 mult 4 1 2 // r4 = r1 * r2
 sll 4 4 0x03 // r4 = r1 << 3
 srl 4 4 0x03 // r4 = r1 >> 3
 loadi 5 0xF0 // r5 = -16
-sla 5 5 0x02 // r5 = r5 << 2
+sla 5 5 0x05 // r5 = r5 << 2
 sra 5 5 0x02 // r5 = r5 >> 2
 loadi 6 0xFD // r5 = -3
 ror 6 6 0x02 // r5 = r5 >> 2 rotate
